070b722f6a626e474a88f1d5a1029aa8d5510df6
Resha Muhamad Aldi <aldiresha35@gmail.com>
2024-01-29 13:02:49 +0700

    fungsijs

---
+++ fungsi.js
@@ -0,0 +1,12 @@
+var x = "25";
+var y = "3.14";
+
+var toNum = parseInt(x);
+var toFloat = parseFloat(y);
+var toString = toNum.toString();
+console.log(toString + toString);
+
+function jumlah(a, b) {
+    return a + b
+}
+alert(jumlah(5,20));
